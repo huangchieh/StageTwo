978d514a1dded2c5e3f14b320ff4c863c301cd2f
Jie Huang <jie.huang@aalto.fi>
2024-07-08 14:21:27 +0300

    Complete Preparations For Two Models: Original NewModel

---
+++ EvaluatePerformace/01_run.sh
@@ -0,0 +1,15 @@
+## Prepare two clean models 
+# cp -r ../Water-bilayer/Ref Original
+# cp -r ../Water-bilayer/L50 NewModel
+
+# mkdir -p Original/train_posnet
+# cp ../Water-bilayer/Ref/train_posnet/best_model.pth  Original/train_posnet
+# mkdir -p Original/train_graphnet
+# cp ../Water-bilayer/Ref/train_graphnet/best_model.pth  Original/train_graphnet
+# 
+# mkdir -p NewModel/train_posnet
+# cp ../Water-bilayer/L50/train_posnet/best_model.pth  NewModel/train_posnet
+# mkdir -p NewModel/train_graphnet
+# cp ../Water-bilayer/L50/train_graphnet/best_model.pth  NewModel/train_graphnet
+
+
---
+++ EvaluatePerformace/NewModel/Prediction_b/01_run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --time=00-05:00:00      # Job time allocation
+#SBATCH --gres=gpu:1            # Request GPUs
+#SBATCH --constraint=ampere|volta # Request specific nodes
+#SBATCH --mem=64G               # Memory
+#SBATCH -c 4                    # Number of cores
+#SBATCH -J Prediction_b        # Job name
+#SBATCH -o log_fit.out          # Output file
+
+# Load environment
+# module load anaconda
+module load mamba gcc
+source activate ml-spm
+export OMP_NUM_THREADS=1
+
+# Print job info
+echo "Job ID: "$SLURM_JOB_ID
+echo "Job Name: "$SLURM_JOB_NAME
+echo "Running on nodes: "$SLURM_JOB_NODELIST
+
+# Run fit script
+rm -r ~/.cache # Sometimes it gets stuck if there are existing builds of cuda extensions
+python test_graphnet.py
