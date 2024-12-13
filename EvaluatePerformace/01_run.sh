7b6934922b3d0c6f21965c96bfe98a0acfb7e2c2
Jie Huang <jie.huang@aalto.fi>
2024-12-13 10:40:48 +0200

    update

--- EvaluatePerformace/01_run.sh
+++ EvaluatePerformace/01_run.sh
@@ -1,31 +1,43 @@
-## Prepare two clean models 
-# cp -r ../Water-bilayer/Ref Original
-# cp -r ../Water-bilayer/L50 NewModel
+# Obtain the model list 
+declare -a model_list  # Declare an array to hold the models
+model_list+=("Ref")  # Append the original model to the array
+basedata=Water-bilayer
+for CYCLEGANDATA in PPAFM2Exp_CoAll
+do
+for LAMBDA1 in 0 10 20 40 50 60 70 80 90 100
+do
+for LAMBDA2 in 0 0.1 1 10
+do 
+for EPOCH in latest
+do 
+case=${CYCLEGANDATA}_L${LAMBDA1}_L${LAMBDA2}_E${EPOCH}
+model=${case}
+model_list+=("$model")  # Append model path to the array
+done done done done
 
-# mkdir -p Original/train_posnet
-# cp ../Water-bilayer/Ref/train_posnet/best_model.pth  Original/train_posnet
-# mkdir -p Original/train_graphnet
-# cp ../Water-bilayer/Ref/train_graphnet/best_model.pth  Original/train_graphnet
-# 
-# mkdir -p NewModel/train_posnet
-# cp ../Water-bilayer/L50/train_posnet/best_model.pth  NewModel/train_posnet
-# mkdir -p NewModel/train_graphnet
-# cp ../Water-bilayer/L50/train_graphnet/best_model.pth  NewModel/train_graphnet
+# Forword pass to get the prediction a, b, c for all trained models
+for model in "${model_list[@]}"
+do
+	echo "$model"
+	optimal_posnet_model=../${basedata}/${model}/train_posnet/best_model.pth
+	optimal_graphnet_model=../${basedata}/${model}/train_graphnet/best_model.pth
+	rsync -a EvaTemplate/ ${model}/
+	cp ${optimal_posnet_model} ${model}/train_posnet/
+	cp ${optimal_graphnet_model} ${model}/train_graphnet/
+	for Prediction in Prediction_a Prediction_b Prediction_c
+	do
+		cd ${model}/${Prediction}
+		dataflag=${basedata}_FB_${model}
+		sed -i "s/DATAFLAG/${dataflag}/g" config.yaml # It only changes the config.yaml in which DATAFLAG is present
+		job=${dataflag}
+		sed -i "s/JOBNAME/${job}/g" 01_run.sh 
+		sbatch 01_run.sh
+		cd -
+	done
+done
 
 # Real Distribution P 
-mkdir -p P 
-cp Original/Prediction_1/predictions/*_ref.xyz P
-
-# # Prediction 1 2 3 
-# mkdir -p Q/Prediction_1 Q/Prediction_2 Q/Prediction_3
-# cp Original/Prediction_1/predictions/*_pred.xyz Q/Prediction_1
-# cp Original/Prediction_2/predictions/*_pred.xyz Q/Prediction_2
-# cp Original/Prediction_3/predictions_Original/*_mol.xyz Q/Prediction_3
-# 
-# # Prediction a b c
-# mkdir -p Q/Prediction_a Q/Prediction_b Q/Prediction_c
-# cp NewModel/Prediction_a/predictions/*_pred.xyz Q/Prediction_a
-# cp NewModel/Prediction_b/predictions/*_pred.xyz Q/Prediction_b
-# cp NewModel/Prediction_c/predictions_NewModel/*_mol.xyz Q/Prediction_c
+#mkdir -p P 
+#cp Original/Prediction_1/predictions/*_ref.xyz P # This is the top layer of water 
 
 
